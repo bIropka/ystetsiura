919c1826dcddaa4a892118fe28bce117cdb3c64e
Yegor Stetsiura <biropka@gmail.com>
2023-07-29 16:29:16 +0200

    desk updated

--- studio/schemas/desk.ts
+++ studio/schemas/desk.ts
@@ -3,16 +3,28 @@ import { StructureBuilder } from 'sanity/desk'
 const structure = (S: StructureBuilder) =>
   S.list()
     .id('general')
+    .title('Content')
     .items([
       S.documentListItem().id('personalData').title('Personal Data').schemaType('personalData'),
-      S.documentListItem().id('aboutMe').title('About Me').schemaType('aboutMe'),
-      S.documentListItem().id('resume').title('Resume').schemaType('resume'),
-      S.documentListItem().id('works').title('Works').schemaType('works'),
-      S.documentListItem().id('blog').title('Blog').schemaType('blog'),
-      S.documentListItem().id('contact').title('Contact').schemaType('contact'),
-      S.documentListItem().id('post').title('Posts').schemaType('post'),
-      S.documentListItem().id('author').title('Authors').schemaType('author'),
-      S.documentListItem().id('category').title('Categories').schemaType('category'),
+      S.divider(),
+      S.listItem()
+        .title('Pages')
+        .child(
+          S.list()
+            .id('pages')
+            .items([
+              S.documentListItem().id('aboutMe').title('About Me').schemaType('aboutMe'),
+              S.documentListItem().id('resume').title('Resume').schemaType('resume'),
+              S.documentListItem().id('works').title('Works').schemaType('works'),
+              S.documentListItem().id('blog').title('Blog').schemaType('blog'),
+              S.documentListItem().id('contact').title('Contact').schemaType('contact'),
+            ])
+        ),
+      S.divider(),
+      S.listItem().title('Posts').child(S.documentTypeList('post')),
+      S.listItem().title('Authors').child(S.documentTypeList('author')),
+      S.listItem().title('Categories').child(S.documentTypeList('category')),
+      S.divider(),
     ])
 
 export default structure
