af75fddba15f34ac11d2e97d8e496a736ef1d4a4
Iegor Stetsiura <iegor.stetsiura@ext.dazn.com>
2023-08-05 11:26:21 +0200

    useAsynData types error resolved

--- app/api/queries.ts
+++ app/api/queries.ts
@@ -1,6 +1,7 @@
 import sanityClient from '~/api/sanity-client'
+import { PersonalDataType } from '~/types'
 
-export const getPersonalData = async () =>
+export const getPersonalData = async (): Promise<PersonalDataType> =>
   await sanityClient.fetch(
     `*[_type == "personalData"][0]{..., socialList[]->, "photo": photo.asset->url}`
   )
--- app/components/layout/HeaderVue.vue
+++ app/components/layout/HeaderVue.vue
@@ -1,31 +1,29 @@
 <script setup lang="ts">
 import { getPersonalData } from '~/api/queries'
-import { PersonalDataType } from '~/types'
 import SocialIcon from '~/components/common/SocialIcon.vue'
 
-const { firstName, lastName, position, email, birthday, location, phone, photo, socialList } =
-  useAsyncData<PersonalDataType>('personal-data', getPersonalData)?.data?.value ?? {}
+const { data } = useAsyncData('personal-data', getPersonalData)
 </script>
 
 <template>
-  <header class="flex bg-surface w-full p-5 col-span-6 rounded-3xl relative -mt-24">
+  <header v-if="data" class="flex bg-surface w-full p-5 col-span-6 rounded-3xl relative -mt-24">
     <div class="w-1/2 pr-7 pl-40 border-br border-solid border-r">
       <div
-        v-if="photo"
+        v-if="data.photo"
         class="w-36 h-36 rounded-[32px] overflow-hidden border-[6px] border-surface absolute left-7 -top-7"
       >
         <img
           class="w-full h-full object-cover object-top"
-          :src="photo"
-          :alt="firstName + ' ' + lastName"
+          :src="data.photo"
+          :alt="data.firstName + ' ' + data.lastName"
         />
       </div>
-      <h2 v-if="firstName && lastName" class="text-2xl font-pmedium text-heading">
-        {{ firstName }} {{ lastName }}
+      <h2 v-if="data.firstName && data.lastName" class="text-2xl font-pmedium text-heading">
+        {{ data.firstName }} {{ data.lastName }}
       </h2>
-      <span v-if="position" class="text-body text-sm">{{ position }}</span>
+      <span v-if="data.position" class="text-body text-sm">{{ data.position }}</span>
       <ul class="flex items-center mt-3">
-        <li v-for="link in socialList" :key="link.url" class="mx-1">
+        <li v-for="link in data.socialList" :key="link.url" class="mx-1">
           <a
             :href="link.url"
             :title="link.title"
@@ -40,19 +38,19 @@ const { firstName, lastName, position, email, birthday, location, phone, photo, 
     <div class="w-1/2 grid grid-cols-2 pl-7">
       <div class="my-2">
         <div class="font-pmedium text-xs uppercase text-body">email</div>
-        <div class="text-sm text-heading mt-1">{{ email }}</div>
+        <div class="text-sm text-heading mt-1">{{ data.email }}</div>
       </div>
       <div class="my-2">
         <div class="font-pmedium text-xs uppercase text-body">Birthday</div>
-        <div class="text-sm text-heading mt-1">{{ birthday }}</div>
+        <div class="text-sm text-heading mt-1">{{ data.birthday }}</div>
       </div>
       <div class="my-2">
         <div class="font-pmedium text-xs uppercase text-body">Phone</div>
-        <div class="text-sm text-heading mt-1">{{ phone }}</div>
+        <div class="text-sm text-heading mt-1">{{ data.phone }}</div>
       </div>
       <div class="my-2">
         <div class="font-pmedium text-xs uppercase text-body">Location</div>
-        <div class="text-sm text-heading mt-1">{{ location }}</div>
+        <div class="text-sm text-heading mt-1">{{ data.location }}</div>
       </div>
     </div>
   </header>
